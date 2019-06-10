2be0c549dbf446ef5f35624080a1585041f82ea1
Edu <uniaodu@gmail.com>
2019-06-10 14:41:56 -0300

    New Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if(sendEmail.resolveActivity(getPackageManager())   != null){
                     startActivity(sendEmail);
+                    //alteração
                 }
 
 
